95dd4b949672b7aabec595ed48ebe059f7f21346
ichater <chaterizaak@gmail.com>
2023-07-07 11:48:26 +1000

    moved Navbar components to ./app directory so it can be used by all route groups

--- src/app/(unauthed)/layout.tsx
+++ src/app/(unauthed)/layout.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import { Inter } from "next/font/google";
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
 import type { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
